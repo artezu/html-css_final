f50de3e733f2b66362dbb78d748b750d4b430027
artezu <artemzuev.spb@yandex.ru>
2020-07-09 22:57:10 +0300

    swipe_mobile

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ function figure(){//Отрисовка фигуры
     grad.addColorStop(0, '#ff2f96');
     ctx.fillStyle = grad;
     ctx.beginPath();
-    ctx.moveTo(300, 755);
-    ctx.bezierCurveTo(-180, 680, 0, 400,300, 100);
+    ctx.moveTo(250, 755);
+    ctx.bezierCurveTo(-180, 680, 0, 400,250, 100);
     ctx.bezierCurveTo(520, -110, 1000, 20,900, 490);
-    ctx.bezierCurveTo(800, 750, 600, 760,300, 755);
+    ctx.bezierCurveTo(800, 750, 600, 760,250, 755);
     ctx.closePath();
     ctx.fill();
 };
@@ -73,6 +73,7 @@ function s_left_class(i1,i2,i3,n){
 };
 $(function(){
     figure();
+    var time=250;//задержка
     $(".ex__list").swipe( {
         swipeStatus:function(event, phase, direction, distance, duration, fingerCount, fingerData, currentDirection)
         {
@@ -99,108 +100,159 @@ $(function(){
                 i2= $("#list_swipe .ex__item").index($(".ex__item_order_2"));
                 
                 if(ww > 1198){//для большого экрана
-                    if(direction == "left"){
-                        s_left_class(i1,i2,i3,n);
+                    if(n > 3){
+                        if(direction == "left"){
+                           
+                            
+                            //s_left_class(i1,i2,i3,n);
+                        }
+                        else if(direction == "right"){
+                            //s_right_class(i1,i2,i3,n);
+                        }
                     }
-                    else if(direction == "right"){
-                        s_right_class(i1,i2,i3,n);
+                    else{
+                        $(".ex__item").animate({
+                            left: "0px"
+                        },time,"linear");
                     }
                 }
                 else if(ww > 979){//для среднего
-                    if(direction == "left"){
-
-                    }
-                    else if(direction == "right"){
-                        if(i1 == 0){//$(".ex__item:eq("+(n-1)+")")
-                            $(".ex__item:eq("+(n-1)+")").css({
+                    if(n > 2){//защита от недостаточного количества эл-тов для пролистывания
+                        if(direction == "left"){
+                            $(".ex__item_order_3").css({
                                 display:"inline-block",
-                                transform:"translate(-590px, 0px)",
+                                "margin-left":"35px",
                                 opacity:0
                             });
+                            $(".ex__item_order_2").css({
+                                "margin-left":"35px"
+                            });
+                            $(".ex__item_order_2").animate({
+                                left: "-457px",
+                            },time,"linear");
                             $(".ex__item_order_1").animate({
-                                left: "280px",
+                                left: "-457px",
                                 opacity:0
-                            },100,"linear");
-                            $(".ex__item:eq("+(n-1)+")").animate({
-                                left: "280px",
+                            },time,"linear");
+                            $(".ex__item_order_3").animate({
+                                left: "-457px",
                                 opacity:1
-                            },100,"linear");
+                            },time,"linear",
+                                function(){s_left_class(i1,i2,i3,n);
+                                    $(".ex__item").attr('style', '');}
+                            );
                         }
+                        else if(direction == "right"){
+                            var n1;
+                            if(i1 == 0)
+                                n1 = n-1;
+                            else
+                                n1 = i1-1;
+
+                            $(".ex__item:eq("+(n1)+")").css({
+                                display:"inline-block",
+                                transform:"translate(-1390px, 0px)",
+                                opacity:0
+                            });
+                            $(".ex__item_order_2").css({
+                                "margin-left":"35px"
+                            });
+                            $(".ex__item_order_1").animate({
+                                left: "495px"
+                            },time,"linear");
+                            $(".ex__item:eq("+(n1)+")").animate({
+                                left: "495px",
+                                opacity:1
+                            },time,"linear");
+                            $(".ex__item_order_2").animate({
+                                left: "495px",
+                                opacity:0
+                            },time,"linear",function(){
+                                s_right_class(i1,i2,i3,n);
+                                $(".ex__item").attr('style', '');
+                            });
+                        }
+                    }
+                    else{
+                        $(".ex__item").animate({
+                            left: "0px"
+                        },time,"linear");
                     }
                 }
                 else{//для маленького
-                    if(direction == "left"){
-                        $(".ex__item_order_2").css({
-                            display:"inline-block",
-                            "margin-left":"10px",
-                            opacity:0
-                        });
-                        $(".ex__item_order_1").animate({
-                            left: "-280px",
-                            "margin-right":"10px",
-                            opacity:0
-                        },100,"linear");
-                        $(".ex__item_order_2").animate({
-                            left: "-280px",
-                            "margin-left":"0px",
-                            opacity:1
-                        },100,"linear", function(){s_left_class(i1,i2,i3,n);
-                            $(".ex__item_order_1").attr('style', '');
-                            $(".ex__item_order_2").attr('style', '');
-                            $(".ex__item_order_3").attr('style', '');});
+                    if(n > 1){//защита от недостаточного количества эл-тов для пролистывания
+                        if(direction == "left"){
+                            $(".ex__item_order_2").css({
+                                display:"inline-block",
+                                "margin-left":"10px",
+                                opacity:0
+                            });
+                            $(".ex__item_order_1").animate({
+                                left: "-280px",
+                                "margin-right":"10px",
+                                opacity:0
+                            },time,"linear");
+                            $(".ex__item_order_2").animate({
+                                left: "-280px",
+                                "margin-left":"0px",
+                                opacity:1
+                            },time,"linear", function(){s_left_class(i1,i2,i3,n);
+                                $(".ex__item").attr('style', '');});
+                        }
+                        else if(direction == "right"){
+                            if(i1 == 0){//$(".ex__item:eq("+(n-1)+")")
+                                $(".ex__item:eq("+(n-1)+")").css({
+                                    display:"inline-block",
+                                    transform:"translate(-590px, 0px)",
+                                    opacity:0
+                                });
+                                $(".ex__item_order_1").animate({
+                                    left: "280px",
+                                    opacity:0
+                                },time,"linear");
+                                $(".ex__item:eq("+(n-1)+")").animate({
+                                    left: "280px",
+                                    opacity:1
+                                },time,"linear", function(){
+                                    s_right_class(i1,i2,i3,n);
+                                    $(".ex__item").attr('style', '');
+                                });
+                            }
+                            else{//$(".ex__item:eq("+(i1-1)+")")
+                                $(".ex__item:eq("+(i1-1)+")").css({
+                                    display:"inline-block",
+                                    transform:"translate(-590px, 0px)",
+                                    opacity:0
+                                });
+                                $(".ex__item_order_1").animate({
+                                    left: "280px",
+                                    opacity:0
+                                },time,"linear");
+                                $(".ex__item:eq("+(i1-1)+")").animate({
+                                    left: "280px",
+                                    opacity:1
+                                },time,"linear", function(){
+                                    s_right_class(i1,i2,i3,n);
+                                    $(".ex__item").attr('style', '');
+                                });
+                            }
+                        }
                     }
-                    else if(direction == "right"){
-                        if(i1 == 0){//$(".ex__item:eq("+(n-1)+")")
-                            $(".ex__item:eq("+(n-1)+")").css({
-                                display:"inline-block",
-                                transform:"translate(-590px, 0px)",
-                                opacity:0
-                            });
-                            $(".ex__item_order_1").animate({
-                                left: "280px",
-                                opacity:0
-                            },100,"linear");
-                            $(".ex__item:eq("+(n-1)+")").animate({
-                                left: "280px",
-                                opacity:1
-                            },100,"linear", function(){
-                                s_right_class(i1,i2,i3,n);
-                                $(".ex__item_order_1").attr('style', '');
-                                $(".ex__item_order_2").attr('style', '');
-                                $(".ex__item_order_3").attr('style', '');
-                            });
-                        }
-                        else{//$(".ex__item:eq("+(i1-1)+")")
-                            $(".ex__item:eq("+(i1-1)+")").css({
-                                display:"inline-block",
-                                transform:"translate(-590px, 0px)",
-                                opacity:0
-                            });
-                            $(".ex__item_order_1").animate({
-                                left: "280px",
-                                opacity:0
-                            },100,"linear");
-                            $(".ex__item:eq("+(i1-1)+")").animate({
-                                left: "280px",
-                                opacity:1
-                            },100,"linear", function(){
-                                s_right_class(i1,i2,i3,n);
-                                $(".ex__item_order_1").attr('style', '');
-                                $(".ex__item_order_2").attr('style', '');
-                                $(".ex__item_order_3").attr('style', '');
-                            });
-                        }
+                    else{
+                        $(".ex__item").animate({
+                            left: "0px"
+                        },time,"linear");
                     }
                 }
             }
             else{
                 $(".ex__item").animate({
                     left: "0px"
-                },100,"linear");
+                },time,"linear");
             }
      },
      allowPageScroll: "horizontal", 
-     triggerOnTouchEnd:false,
-     threshold:100 // сработает через 100 пикселей
+     triggerOnTouchEnd: true,
+     threshold:80 // сработает, если пройдено 80 пикселей
     });
 });
