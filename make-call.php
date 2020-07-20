c6a5ae92124924fc2d23023bd9919470436bd5af
artezu <artemzuev.spb@yandex.ru>
2020-07-20 14:19:45 +0300

    good 1.2

--- make-call.php
+++ make-call.php
@@ -1,7 +1,7 @@
 <?php
     mail("info@awebd.ru",
-    "Новая заявка",
-    "<h4>Подробнее</h4>
+    "Новая заявка - Обратный звонок",
+    "<h4>Обратный звонок</h4>
     <p>Имя: ".$_POST['name']."</p>
     <p>Телефон: ".$_POST['number']."</p>",
      "From: Awebd.ru <info@awebd.ru>\r\nContent-type: text/html; charset=utf-8 \r\n"); 
